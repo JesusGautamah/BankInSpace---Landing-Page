1238a3bf394e04e29ec8f5f2b403c5dc2a55ed14
JesusGautamah <lima.jesuscc@gmail.com>
2021-11-02 08:59:45 -0300

    hotfix: package name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsDocker
+module BankInSpace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
